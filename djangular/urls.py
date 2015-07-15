f2860f00bf1b9d56e4ba3f9d3fd918cf22271760
Boey Pak Cheong <pcboey@yahoo.com>
2015-07-16 00:02:41 +0800

    Added books_url and index.html

--- djangular/urls.py
+++ djangular/urls.py
@@ -17,7 +17,12 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
+
+from books import urls as books_urls
 
 urlpatterns = [
+    url(r'^$', TemplateView.as_view(template_name='index.html')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^books/', include(books_urls)),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
