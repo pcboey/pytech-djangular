22f30163c9338f9831542d9034f26c38ce4f1a53
Boey Pak Cheong <pcboey@yahoo.com>
2015-07-15 18:27:43 +0800

    Added settings to handle media

--- djangular/settings.py
+++ djangular/settings.py
@@ -115,6 +115,10 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 TEMPLATE_DIRS = (
     os.path.join(BASE_DIR, 'templates'),
 )
+
