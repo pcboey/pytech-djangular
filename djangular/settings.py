d3bd83afd403790ea94689d5f79e01e9663e3320
Boey Pak Cheong <pcboey@yahoo.com>
2015-07-15 20:15:07 +0800

    Corrected typo cririspy_forms

--- djangular/settings.py
+++ djangular/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bootstrap3_datetime',
-    'cripsy_forms',
+    'crispy_forms',
     'country',
     'books',
 )
